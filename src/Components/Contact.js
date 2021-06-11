6793e12faf1a32ef2d9ab49ea4e0cdc318a61ff7
Sunny Modi <sunnymodi21@gmail.com>
2021-06-11 18:27:57 -0500

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -10,8 +10,8 @@ class Contact extends Component {
       var state = this.props.data.address.state;
       var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
-      var email = this.props.data.email;
-      var message = this.props.data.contactmessage;
+      // var email = this.props.data.email;
+      // var message = this.props.data.contactmessage;
     }
 
     return (
